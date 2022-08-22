3037fe89f4b582924b08da6289c3910d656d7ce9
krasholena <krasholena@gmail.com>
2022-08-22 15:07:32 +0200

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Max   float32
+	Items []Item
+}
 
 // Item stores item information
+type Item struct {
+	Description string
+	Price       float32git add . && git commit -m 'Completed module'
+}
